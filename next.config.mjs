113d06e65cf1ea474646e439c7e1f2691fcb5098
Kenil <kenilpatel0230@gmail.com>
2026-07-22 08:49:56 +0530

    Update basePath for new repo

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: "export",
-  basePath: process.env.CF_PAGES ? "" : process.env.NODE_ENV === "production" ? "/Demo_Website" : "",
+  basePath: process.env.CF_PAGES ? "" : process.env.NODE_ENV === "production" ? "/DemoWebsite" : "",
   compiler: {
     removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
   },
